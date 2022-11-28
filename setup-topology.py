fcfca1c444988052aebfc14c2802f63f2e0c8955
Abdoueck632 <abdouseck632@gmail.com>
2022-11-28 13:24:25 +0100

    resolve mininet bugs

--- setup-topology.py
+++ setup-topology.py
@@ -2,105 +2,96 @@
 from mininet.net import Mininet
 from mininet.cli import CLI
 from mininet.link import Link, TCLink, Intf
-from subprocess import Popen, PIPE
-from mininet.log import setLogLevel
 from mininet.node import Controller
 from mininet.topo import Topo
 if '__main__' == __name__:
-    setLogLevel('info')
+        
     net = Mininet(link=TCLink)
-    key = "net.mptcp.mptcp_enabled"
-    value = 1
-    p = Popen("sysctl -w %s=%s" % (key, value),
-            shell=True, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = p.communicate()
-    print ("stdout=", stdout, "stderr=", stderr)
-    
     
     c0 = net.addController("controller")
 
-    s1 = net.addSwitch('s1')
-    s2 = net.addSwitch('s2')
-    s3 = net.addSwitch('s3')
-    s4 = net.addSwitch('s4')
+    sw1 = net.addSwitch('sw1')
+    sw2 = net.addSwitch('sw2')
+    sw3 = net.addSwitch('sw3')
+    sw4 = net.addSwitch('sw4')
     
-    h1 = net.addHost('S1')
-    h2 = net.addHost('R1')
-    h4 = net.addHost('R2')
-    h3 = net.addHost('C1')
-    r1 = net.addHost('r1')
+    Server = net.addHost('S') 
+    Relay1 = net.addHost('R1')
+    Relay2 = net.addHost('R2')
+    Client = net.addHost('C')
+    router = net.addHost('router')
     linkopt = {'bw': 10}
     linkopt2 = {'bw': 100}
     
-    net.addLink(r1, s1, cls=TCLink, **linkopt)
-    net.addLink(r1, s2, cls=TCLink, **linkopt)
-    net.addLink(r1, s3, cls=TCLink, **linkopt)
-    net.addLink(r1, s4, cls=TCLink, **linkopt2)
+    net.addLink(router, sw1, cls=TCLink, **linkopt)
+    net.addLink(router, sw2, cls=TCLink, **linkopt)
+    net.addLink(router, sw3, cls=TCLink, **linkopt)
+    net.addLink(router, sw4, cls=TCLink, **linkopt2)
 
-    net.addLink(s1, h1, cls=TCLink, **linkopt)
-    net.addLink(s2, h1, cls=TCLink, **linkopt)
-    net.addLink(s3, h2, cls=TCLink, **linkopt)
-    net.addLink(s3, h4, cls=TCLink, **linkopt)
-    net.addLink(s4, h3, cls=TCLink, **linkopt2)
+    net.addLink(sw1, Server, cls=TCLink, **linkopt)
+    net.addLink(sw2, Server, cls=TCLink, **linkopt)
+    net.addLink(sw3, Relay1, cls=TCLink, **linkopt)
+    net.addLink(sw3, Relay2, cls=TCLink, **linkopt)
+    net.addLink(sw4, Client, cls=TCLink, **linkopt2)
     net.build()
-    r1.cmd("ifconfig r1-eth0 0")
-    r1.cmd("ifconfig r1-eth1 0")
-    r1.cmd("ifconfig r1-eth2 0")
+    router.cmd("ifconfig router-eth0 0")
+    router.cmd("ifconfig router-eth1 0")
+    router.cmd("ifconfig router-eth2 0")
 
-    h1.cmd("ifconfig S1-eth0 0")
-    h1.cmd("ifconfig S1-eth1 0")
+    Server.cmd("ifconfig S-eth0 0")
+    Server.cmd("ifconfig S-eth1 0")
     
-    h2.cmd("ifconfig R1-eth0 0")
+    Relay1.cmd("ifconfig R1-eth0 0")
     
-    h4.cmd("ifconfig R2-eth0 0")
+    Relay2.cmd("ifconfig R2-eth0 0")
 
-    h3.cmd("ifconfig C1-eth0 0")
+    Client.cmd("ifconfig C-eth0 0")
     
-    r1.cmd("echo 1 > /proc/sys/net/ipv4/ip_forward")
-    r1.cmd("ifconfig r1-eth0 10.0.0.1 netmask 255.255.255.0")
-    r1.cmd("ifconfig r1-eth1 10.0.1.1 netmask 255.255.255.0")
-    r1.cmd("ifconfig r1-eth2 10.0.2.1 netmask 255.255.255.0")
-    r1.cmd("ifconfig r1-eth3 10.0.3.1 netmask 255.255.255.0")
+    router.cmd("echo 1 > /proc/sys/net/ipv4/ip_forward")
+    router.cmd("ifconfig router-eth0 10.0.0.1 netmask 255.255.255.0")
+    router.cmd("ifconfig router-eth1 10.0.1.1 netmask 255.255.255.0")
+    router.cmd("ifconfig router-eth2 10.0.2.1 netmask 255.255.255.0")
+    router.cmd("ifconfig router-eth3 10.0.3.1 netmask 255.255.255.0")
     
-    h1.cmd("ifconfig S1-eth0 10.0.0.2 netmask 255.255.255.0")
-    h1.cmd("ifconfig S1-eth1 10.0.1.2 netmask 255.255.255.0")
+    Server.cmd("ifconfig S-eth0 10.0.0.2 netmask 255.255.255.0")
+    Server.cmd("ifconfig S-eth1 10.0.1.2 netmask 255.255.255.0")
     
-    h2.cmd("ifconfig R1-eth0 10.0.2.2 netmask 255.255.255.0")
+    Relay1.cmd("ifconfig R1-eth0 10.0.2.2 netmask 255.255.255.0")
 
-    h4.cmd("ifconfig R2-eth0 10.0.2.3 netmask 255.255.255.0")
+    Relay2.cmd("ifconfig R2-eth0 10.0.2.3 netmask 255.255.255.0")
     
-    h3.cmd("ifconfig C1-eth0 10.0.3.2 netmask 255.255.255.0")
+    Client.cmd("ifconfig C-eth0 10.0.3.2 netmask 255.255.255.0")
     
-    h1.cmd("ip rule add from 10.0.0.2 table 1")
-    h1.cmd("ip rule add from 10.0.1.2 table 2")
-    h1.cmd("ip route add 10.0.0.0/24 dev S1-eth0 scope link table 1")
-    h1.cmd("ip route add default via 10.0.0.1 dev S1-eth0 table 1")
-    h1.cmd("ip route add 10.0.1.0/24 dev S1-eth1 scope link table 2")
-    h1.cmd("ip route add default via 10.0.1.1 dev S1-eth1 table 2")
-    h1.cmd("ip route add default scope global nexthop via 10.0.0.1 dev S1-eth0")
+    Server.cmd("ip rule add from 10.0.0.2 table 1")
+    Server.cmd("ip rule add from 10.0.1.2 table 2")
+    Server.cmd("ip route add 10.0.0.0/24 dev S-eth0 scope link table 1")
+    Server.cmd("ip route add default via 10.0.0.1 dev S-eth0 table 1")
+    Server.cmd("ip route add 10.0.1.0/24 dev S-eth1 scope link table 2")
+    Server.cmd("ip route add default via 10.0.1.1 dev S-eth1 table 2")
+    Server.cmd("ip route add default scope global nexthop via 10.0.0.1 dev S-eth0")
     
-    h2.cmd("ip rule add from 10.0.2.2 table 1")
-    h2.cmd("ip route add 10.0.2.0/24 dev R1-eth0 scope link table 1")
-    h2.cmd("ip route add default via 10.0.2.1 dev R1-eth0 table 1")
-    h2.cmd("ip route add default scope global nexthop via 10.0.2.1 dev R1-eth0")
+    Relay1.cmd("ip rule add from 10.0.2.2 table 1")
+    Relay1.cmd("ip route add 10.0.2.0/24 dev R1-eth0 scope link table 1")
+    Relay1.cmd("ip route add default via 10.0.2.1 dev R1-eth0 table 1")
+    Relay1.cmd("ip route add default scope global nexthop via 10.0.2.1 dev R1-eth0")
 
-    h4.cmd("ip rule add from 10.0.2.3 table 1")
-    h4.cmd("ip route add 10.0.2.0/24 dev R2-eth0 scope link table 1")
-    h4.cmd("ip route add default via 10.0.2.1 dev R2-eth0 table 1")
-    h4.cmd("ip route add default scope global nexthop via 10.0.2.1 dev R2-eth0")
+    Relay2.cmd("ip rule add from 10.0.2.3 table 1")
+    Relay2.cmd("ip route add 10.0.2.0/24 dev R2-eth0 scope link table 1")
+    Relay2.cmd("ip route add default via 10.0.2.1 dev R2-eth0 table 1")
+    Relay2.cmd("ip route add default scope global nexthop via 10.0.2.1 dev R2-eth0")
     
-    h3.cmd("ip rule add from 10.0.3.2 table 1")
-    h3.cmd("ip route add 10.0.3.0/24 dev C1-eth0 scope link table 1")
-    h3.cmd("ip route add default via 10.0.3.1 dev C1-eth0 table 1")
-    h3.cmd("ip route add default scope global nexthop via 10.0.3.1 dev C1-eth0")
+    Client.cmd("ip rule add from 10.0.3.2 table 1")
+    Client.cmd("ip route add 10.0.3.0/24 dev C-eth0 scope link table 1")
+    Client.cmd("ip route add default via 10.0.3.1 dev C-eth0 table 1")
+    Client.cmd("ip route add default scope global nexthop via 10.0.3.1 dev C-eth0")
     
     controller = net.controllers[0]
     controller.start()
     
-    s1.start([controller])
-    s2.start([controller])
-    s3.start([controller])
-    s4.start([controller])
+    sw1.start([controller])
+    sw2.start([controller])
+    sw3.start([controller])
+    sw4.start([controller])
 
     CLI(net)
     net.stop()
